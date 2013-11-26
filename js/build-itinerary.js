713c8563912654ef12e65250576f959f54a848f1
Kaitlin Huben <kjh2141@columbia.edu>
2013-11-26 11:00:41 -0500

    Moved navbar->js for consistency, added template
    main.js populates the navbar for consistency across pages
    Added bootstrap template for future pages
    Removed old testing.html

--- js/build-itinerary.js
+++ js/build-itinerary.js
@@ -1,9 +1,10 @@
+/** Javascript functionality for the itinerary builder page */
 $(function() {
 	$("#holder").html("jQuery ready");
 	var urlToSend = "";
 	$.ajax({
-	  url: "https://api.foursquare.com/v2/venues/4abf1d17f964a520d39020e3?client_id=5CYXNIKAOPTKCKIGHNPPJ3DQJBY4IPL0XJL140TLN121U514&client_secret=RPZTJ5NHBY0L213UKWP3T3DF2QVUXNKMW34FRJOUZFDIFNDM&v=20131124",
-	  //url: "https://api.foursquare.com/v2/venues/search?client_id=5CYXNIKAOPTKCKIGHNPPJ3DQJBY4IPL0XJL140TLN121U514&client_secret=RPZTJ5NHBY0L213UKWP3T3DF2QVUXNKMW34FRJOUZFDIFNDM&v=20131124&near=New%20York&query=MoMa",
+	  //url: "https://api.foursquare.com/v2/venues/4abf1d17f964a520d39020e3?client_id=5CYXNIKAOPTKCKIGHNPPJ3DQJBY4IPL0XJL140TLN121U514&client_secret=RPZTJ5NHBY0L213UKWP3T3DF2QVUXNKMW34FRJOUZFDIFNDM&v=20131124",
+	  url: "https://api.foursquare.com/v2/venues/search?client_id=5CYXNIKAOPTKCKIGHNPPJ3DQJBY4IPL0XJL140TLN121U514&client_secret=RPZTJ5NHBY0L213UKWP3T3DF2QVUXNKMW34FRJOUZFDIFNDM&v=20131124&near=New%20York&query=MoMa",
 	  beforeSend: function() {
 		// can put something here
 	  }
